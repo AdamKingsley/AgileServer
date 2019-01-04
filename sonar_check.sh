ec126cbff917b6d77ae8200ff579f4c7cd6992d6
AdamKingsley <141250133@smail.nju.edu.cn>
2019-01-04 17:06:13 +0800

    sonar commit

---
+++ sonar_check.sh
@@ -0,0 +1,5 @@
+mvn sonar:sonar \
+  -Dsonar.projectKey=AdamKingsley_AgileServer \
+  -Dsonar.organization=adamkingsley-github \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=28a2185cf873ca88cc1aae489249e53e36beb133
