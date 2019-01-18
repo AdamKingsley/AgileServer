ad1fce62758413bce89824a4debf197694099b9c
AdamKingsley <desperatewilder@gmail.com>
2019-01-18 11:42:30 +0800

    debug：添加SQL查询

--- src/main/java/cn/edu/nju/software/agile_server/dao/TourRepository.java
+++ src/main/java/cn/edu/nju/software/agile_server/dao/TourRepository.java
@@ -3,14 +3,16 @@ package cn.edu.nju.software.agile_server.dao;
 import cn.edu.nju.software.agile_server.entity.Tour;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TourRepository extends JpaRepository<Tour,Long> {
 
-    Tour findByIdAndState(Long tourId, Integer state);
+    Tour findByIdAndState(Long id, Integer state);
 
     List<Tour> findByState(Integer state);
 
+    @Query(value = "select * from #{#entityName} as t where t.clubId in ?1 and t.state=?2",nativeQuery = true)
     List<Tour> findAllByClubIdExistsAndState(List<Long> clubIds, Integer state);
 }
