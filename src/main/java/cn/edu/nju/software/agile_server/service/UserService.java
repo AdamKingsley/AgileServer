b10f12d40d4bd38be66254a537eda98a8380caa3
haoran <1330486645@qq.com>
2019-04-06 12:09:37 +0800

    可以修改个人简介

--- src/main/java/cn/edu/nju/software/agile_server/service/UserService.java
+++ src/main/java/cn/edu/nju/software/agile_server/service/UserService.java
@@ -1,7 +1,10 @@
 package cn.edu.nju.software.agile_server.service;
 
 import cn.edu.nju.software.agile_server.common.Result;
+import cn.edu.nju.software.agile_server.entity.Notification;
 import cn.edu.nju.software.agile_server.form.LoginForm;
+import cn.edu.nju.software.agile_server.form.NotificationForm;
+import cn.edu.nju.software.agile_server.form.UserCommentForm;
 import cn.edu.nju.software.agile_server.form.UserForm;
 
 public interface UserService {
@@ -12,4 +15,10 @@ public interface UserService {
     Result update(UserForm userForm);
 
     Result getUser(Long userid);
+
+    Result getnotice(Long userid);
+
+    Result updatenotification(NotificationForm notificationForm);
+
+    Result updatecomment(UserCommentForm userCommentForm);
 }
